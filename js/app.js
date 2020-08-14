26426ede27827cc7531544f6195c46191311d3e7
uurunov <68825387+uurunov@users.noreply.github.com>
2020-08-14 19:45:51 +0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -17,19 +17,23 @@
  * Define Global Variables
  *
 */
+
+const section1 = document.querySelector("#section1");
+const section2 = document.querySelector("#section2");
+const section3 = document.querySelector("#section3");
+const section4 = document.querySelector("#section4");
+const container = [section1, section2, section3, section4];
 const section_div = document.querySelectorAll(".landing__container");
 const navbar_list = document.querySelector("#navbar__list");
-const navbar_list_items = navbar_list.childNodes;
-const num_of_navbar_list_items = section_div.length;
-const sections = document.querySelectorAll("section");
 const myFragment = document.createDocumentFragment();
+const navbar_list_items = navbar_list.childNodes;
 const section_bounding = [
 	section_div[0].getBoundingClientRect(),
 	section_div[1].getBoundingClientRect(),
 	section_div[2].getBoundingClientRect(),
 	section_div[3].getBoundingClientRect()
 ];
-const pageBottom = document.querySelector(".page__footer");
+const footer = document.querySelector(".page__footer");
 
 /**
  * End Global Variables
@@ -37,10 +41,20 @@ const pageBottom = document.querySelector(".page__footer");
  *
 */
 
-function navLinkState(theLink, numOfLinks) {
-	for (let i=0; i<numOfLinks; i++)
+const isInViewport = function (elem) {
+	const distance = elem.getBoundingClientRect();
+	return (
+		distance.top >= 0 &&
+		distance.left >= 0 &&
+		distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+		distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+};
+
+function navLinkState(theLink) {
+	for (let i=0; i<section_div.length; i++)
 	{
-		if (navbar_list_items[i] === theLink)
+		if (navbar_list_items[i].textContent === theLink)
 		{
 			navbar_list_items[i].classList.add("menu__link__active");
 		} else {
@@ -49,20 +63,28 @@ function navLinkState(theLink, numOfLinks) {
 	}
 }
 
+function isAtEnd(element) {
+	const distance = element.getBoundingClientRect();
+	return (
+		distance.top <= 750 &&
+		distance.left >= 0 &&
+		distance.bottom <= 900 &&
+		distance.right >= 0
+	);
+}
+
 function addButtonToTop() {
-	const scroll = document.documentElement.scrollHeight - window.innerHeight;
-	const bottomPage = window.scrollY;
-	if (scroll >= bottomPage)
-	{
-		const topButton = document.createElement("p");
-		topButton.textContent = "Go Top";
-		topButton.classList.add("footer__button");
-		topButton.addEventListener("click", function()
-		{window.scrollTo({top:0, left:0, behavior: 'smooth'});});
-		pageBottom.appendChild(topButton);
-	} else {
-		document.querySelector(".footer__button").remove();
-	}
+	const topButton = document.createElement("p");
+	topButton.textContent = "Go Top";
+	topButton.classList.add("footer__button");
+	topButton.addEventListener("click", function(){
+		window.scrollTo({
+			top:0,
+			left:0,
+			behavior: 'smooth'
+		});
+	});
+	footer.appendChild(topButton);
 }
 
 /**
@@ -72,9 +94,10 @@ function addButtonToTop() {
 */
 
 // build the nav
-const buildNav = function (number_of_items) {
 
-	for (let list_item = 1; list_item <= number_of_items; list_item++)
+const buildNav = function () {
+
+	for (let list_item = 1; list_item <= section_div.length; list_item++)
 	{
 		// Creates a new list item element
 		const new_list_item = document.createElement('li');
@@ -93,26 +116,28 @@ const buildNav = function (number_of_items) {
 	navbar_list.appendChild(myFragment);
 }
 
+
 // Add class 'active' to section when near top of viewport
-const addActiveClass = function (index)
+
+const addActiveClass = function (element)
 {
-	sections[index].classList.add("your-active-class");
+	element.classList.add("your-active-class");
 }
 
 // Scroll to anchor ID using scrollTO event
+
 function scrollToTarget(event)
 {
-	for (let i=0; i<num_of_navbar_list_items; i++)
+	for (let i=0; i<section_div.length; i++)
 	{
 		if (event.target.textContent === section_div[i].parentElement.dataset.nav)
 		{
-			navLinkState(event.target, num_of_navbar_list_items);
 			window.scrollTo({
-				top:section_bounding[i].top + 50,
+				top:section_bounding[i].top,
 				left:section_bounding[i].left,
 				behavior: 'smooth'
 			});
-			addActiveClass(i);
+			addActiveClass(section_div[i].parentElement);
 			break;
 		}
 	}
@@ -125,14 +150,22 @@ function scrollToTarget(event)
 */
 
 // Build menu
-document.addEventListener("DOMContentLoaded", buildNav(num_of_navbar_list_items));
+
+document.addEventListener("DOMContentLoaded", buildNav());
 
 // Scroll to section on link click
+
 navbar_list.addEventListener('click', scrollToTarget);
 
 // Set sections as active
-window.addEventListener('scroll', addActiveClass(0));
-window.addEventListener('scroll', addButtonToTop());
 
-
-
+window.addEventListener('scroll', function (event) {
+	for (let member of container)
+	{
+		if (isInViewport(member)) {
+			addActiveClass(member);
+			navLinkState(member.dataset.nav);
+			break;
+		}
+	}
+}, false);
